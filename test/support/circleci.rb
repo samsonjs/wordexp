a67b688f8962246b129ecb2689f5a2ab871ec682
Sami Samhuri <sami@samhuri.net>
2026-01-01 16:37:15 -0800

    Fix CircleCI (#157)
    * Drop Ruby 3.1 from CI
    
    * Fix CircleCI report dir

--- test/support/circleci.rb
+++ test/support/circleci.rb
@@ -1,2 +1,6 @@
 # Generate XML test reports that can be parsed by CircleCI
-require 'minitest/ci' if ENV['CIRCLECI']
+if ENV['CIRCLECI']
+  require 'minitest/ci'
+
+  Minitest::Ci.report_dir = './reports'
+end
